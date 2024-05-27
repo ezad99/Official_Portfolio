3f002f687b7ea07027d9e10e645798df03b91ad5
Ezad99 <ezadhaziq99@gmail.com>
2024-05-27 12:07:42 +0100

    feat(navbar): add navbar to components

---
+++ Official Portfolio/src/Components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {Link} from "react-router-dom"
+
+export const Navbar: React.FC = () => {
+    return (
+        < nav >
+            <Link to="/">Website</Link>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to ="/works">Works</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
---
+++ Official Portfolio/src/Pages/Blog.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Blog
