89875a2fc174d5dbd020715a6f8dcbb42d8aa0bf
Ezad99 <ezadhaziq99@gmail.com>
2024-05-28 16:18:57 +0100

    feat(navbar): add side menu when screen is less then 480px

--- Official Portfolio/src/Components/Navbar.tsx
+++ Official Portfolio/src/Components/Navbar.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link, NavLink } from "react-router-dom"
 
 import "./Navbar.css"
 
 export const Navbar: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
     return (
         < nav >
             <Link to="/home" className='title'>Website</Link>
-            <ul>
+            <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
                 <li><NavLink to="/home">Home</NavLink></li>
                 <li><NavLink to="/works">Works</NavLink></li>
                 <li><NavLink to="/contact">Contact</NavLink></li>
