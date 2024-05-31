2f40e0ae56aee47001a3fd8b5ae8bc09873a5663
Ezad99 <ezadhaziq99@gmail.com>
2024-05-31 21:37:03 +0100

    fix(subdirectory): add subdirectory to all deployment links

--- Official Portfolio/src/App.tsx
+++ Official Portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "./Components/Navbar"
 import { Home, About, Works, Contact, ProjectDetail } from "./Pages"
 
@@ -7,13 +7,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/works" element={<Works />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/project/:id" element={<ProjectDetail />} />
-      </Routes>
+      <Router basename="Official Portfolio">
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/project/:id" element={<ProjectDetail />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
